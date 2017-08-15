0e4b0877428b4b45b84b71ea955cb14db02c670e
odessabear <bearodessa@gmail.com>
2017-08-15 22:24:41 +0300

    Added all new files

---
+++ src/t7/treadsPrograns/CopyFileImpl.java
@@ -0,0 +1,36 @@
+package t7.treadsPrograns;
+
+import t6.FileCopyUtils;
+import t6.IOtheme;
+
+/**
+ * Created by X501A on 15.08.2017.
+ */
+public class CopyFileImpl extends AbstractTask implements CopyFileTask {
+    private String pathFrom;
+    private String pathTo;
+    private FileCopyUtils copyUtils;
+
+    public CopyFileImpl(String pathFrom, String pathTo, FileCopyUtils copyUtils) {
+        this.pathFrom = pathFrom;
+        this.pathTo = pathTo;
+        this.copyUtils = copyUtils;
+
+    }
+
+    @Override
+    public void setFileCopyUtils(FileCopyUtils copyUtils) {
+        this.copyUtils = copyUtils;
+
+    }
+
+    @Override
+    public void execute() throws TaskExecutionFailedException {
+        try {
+            copyUtils.copyFile(pathFrom, pathTo);
+        } catch (Exception e) {
+           throw new TaskExecutionFailedException(e.getMessage());
+        }
+
+    }
+}
---
+++ src/t7/treadsPrograns/CopyFileTask.java
@@ -0,0 +1,19 @@
+package t7.treadsPrograns;
+
+
+import t6.FileCopyUtils;
+
+/**
+ * Задача копирования файла в отдельном потоке
+ * 
+ * @author nedis
+ * @version 1.0
+ */
+public interface CopyFileTask extends Task {
+	/**
+	 * Устанавливает алгоритм копирования файлов. созданный в предыдущем задании
+	 * 
+	 * @param copyUtils алгоритм копирования файлов
+	 */
+	void setFileCopyUtils(FileCopyUtils copyUtils);
+}
